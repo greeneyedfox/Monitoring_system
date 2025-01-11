413f4d12b949637dd47d8328067b48b445fb68b9
greeneyedfox <68673632+greeneyedfox@users.noreply.github.com>
2025-01-11 15:05:34 +0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QApplication
+from qasync import QEventLoop
+from app.gui.main_window import MainWindow
+import asyncio
+
+
+async def main():
+    app = QApplication([])
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
+    # Инициализация основного окна
+    window = MainWindow()
+    window.show()
+
+    # Запуск событийного цикла
+    loop.run_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
